c027492fd992a7b8de901716173b55eed33776c2
praju-nikam <sawanprajakta84@gmail.com>
2022-05-31 16:51:37 +0530

    wel come to address book system

---
+++ src/com/bridgelabz/AddressBookSystemMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class AddressBookSystemMain {
+    public static void main(String[] args) {
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*- Wel to AddressBook System -*-*-*-*-*-*-*-*-*-*-*-*-*");
+
+    }
+}
---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,89 @@
+package com.bridgelabz;
+
+public class Contact
+{
+   private  String firstName,lastName,address,city,state,emailId;
+   private  int zip;
+   private long phoneNumber;
+
+   // Used constructor to store parameters
+
+    public Contact(String firstName, String lastName, String address, String city, String state, String emailId,
+                   int zip, long phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.emailId = emailId;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+    }
+
+    //Used getters And setters to get and set contact details
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
